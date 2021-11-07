eb07ac2b5650c57dc797a3d91610c651495358d4
maplesyrupman <maplesyrupman@protonmail.com>
2021-11-07 13:01:52 -0500

    parties table

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
+CREATE TABLE parties (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    description TEXT
+);
+
 CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
